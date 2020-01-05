2eada79a6ada9187aef8cb16600a7da35c36e8d1
Serdar Mustafa <serdarmustafa1@gmail.com>
2020-01-05 16:01:03 +0200

    solved bug in collectionpreview

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import { Switch, Route } from 'react-router-dom';
 
 import Homepage from './pages/HomePage/HomePage';
 import ShopPage from './pages/ShopPage/ShopPage';
-
+import Header from './components/HeaderComponent/HeaderComponent';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route path="/shop" component={ShopPage} />
--- src/components/CollectionItem/CollectionItem.js
+++ src/components/CollectionItem/CollectionItem.js
@@ -2,14 +2,22 @@ import React from 'react';
 
 import './CollectionItemStyles.scss';
 
-const CollectionItem = ({ id, name, price, imageUrl }) => (
-  <div className="collection-item">
-    <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
-    <div className="collection-footer">
-      <span className="name">{name}</span>
-      <span className="price">{price}</span>
+const CollectionItem = ({ id, name, price, imageUrl }) => {
+  console.log('CollectionItem', imageUrl);
+  return (
+    <div className="collection-item">
+      <div
+        className="image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+      <div className="collection-footer">
+        <span className="name">{name}</span>
+        <span className="price">{price}</span>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default CollectionItem;
