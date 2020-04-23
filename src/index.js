794f1c11833cb2ea1fdb0ddff23abfd6eec7c035
Serdar Mustafa <serdarmustafa@SerdarPsonalMBP.lan>
2020-04-23 11:27:05 +0300

    set up config for absolute imports

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-
 import './index.css';
 import App from './App';
+import store from 'store/store';
 
 ReactDOM.render(
-  <Provider>
+  <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
