0292727cbc54b5437f819bf3fb8b221daec1f6ad
Serdar Mustafa <serdarmustafa@Serdar-Personal-MBP.local>
2020-04-27 09:40:06 +0300

    created cart item component and connect to cart item reducer

--- src/components/CollectionItem/CollectionItem.js
+++ src/components/CollectionItem/CollectionItem.js
@@ -8,7 +8,6 @@ import './CollectionItemStyles.scss';
 
 const CollectionItem = ({ item, addItem }) => {
   const { name, price, imageUrl } = item;
-
   return (
     <div className="collection-item">
       <div
