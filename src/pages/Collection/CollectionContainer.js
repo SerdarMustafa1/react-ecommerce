4118bf9e0e2989f8f243a4566cd78f12ac994a18
Serdar Mustafa <serdar@bolt.ee>
2020-05-26 10:12:26 +0300

    refactored collectionpage styles to outside container

--- src/pages/Collection/CollectionContainer.js
+++ src/pages/Collection/CollectionContainer.js
@@ -2,9 +2,9 @@ import { connect } from 'react-redux';
 import { compose } from 'redux';
 import { createStructuredSelector } from 'reselect';
 
-import { selectIsCollectionsLoaded } from 'redux/shop/shop.selectors';
-import WithSpinner from 'components/WithSpinner/WithSpinnerComponent';
-import CollectionPage from './CollectionPage';
+import { selectIsCollectionsLoaded } from '../../redux/shop/shop.selectors';
+import WithSpinner from '../../components/with-spinner/with-spinner.component';
+import CollectionPage from './collection.component';
 
 const mapStateToProps = createStructuredSelector({
   isLoading: state => !selectIsCollectionsLoaded(state),
